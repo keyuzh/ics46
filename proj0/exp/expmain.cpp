dcd00177977e1ec20b99a6a5a4d5fb932c16e77f
keyuzh <keyuz4@uci.edu>
2021-01-07 19:25:48 -0800

    proj0 complete

--- proj0/exp/expmain.cpp
+++ proj0/exp/expmain.cpp
@@ -5,23 +5,9 @@
 // that are part of the project, outside of the context of the main
 // application or Google Test.
 
-#include "String.hpp"
-#include <iostream>
-
 
 int main()
 {
-    String s;
-
-    char b = '\0';
-    char a = 0;
-
-    if (a == b)
-    {
-        
-        std::cout << "0 == \\0" << std::endl;
-    }
-    
 
     return 0;
 }
